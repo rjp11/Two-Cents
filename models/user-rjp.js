889b41d7005f3aa751f9a6aa09ce3cf76e0e23a8
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-16 18:27:54 -0500

    copied passport example resource from project 2 to start setting up authentication

---
+++ models/user-rjp.js
@@ -0,0 +1,18 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_Name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return User;
+};
--- server.js
+++ server.js
@@ -4,19 +4,25 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
-var db = require("./models");
+// for sequelize ORM
+const db = require("./models");
+// for passport and authentication
+const session = require("express-session");
+const passport = require("./config/passport");
 
 // Serve up static assets
 app.use(express.static("client/build"));
-
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-
 // Use apiRoutes
-app.use("/api", apiRoutes); //localhost:3000/api/...
+app.use("/api", apiRoutes);
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Send every request to the React app
 // Define any API routes before this runs
