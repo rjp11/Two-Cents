299fc48e86e75780ac5bd02208a0bac417836911
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-17 18:22:56 -0500

    changed server port from 3000 to 3001

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return(
             <div>
-                <img alt="news stand in Catania, Sicily" src='https://farm5.staticflickr.com/4477/37543488160_a487b3f012_b.jpg' />
+                <img alt="News stand in Catania, Sicily" src='https://farm5.staticflickr.com/4477/37543488160_a487b3f012_b.jpg' />
             </div>
         )
     }
