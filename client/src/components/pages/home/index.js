ad04e847cfe8f7ffe7c37180542636ef0800d7d6
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-16 19:31:01 -0500

    removed unused axios reference

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Home extends Component {
     render() {
         return(
-            <img src='https://farm5.staticflickr.com/4477/37543488160_a487b3f012_b.jpg' />
+            <img alt="news stand in Catania, Sicily" src='https://farm5.staticflickr.com/4477/37543488160_a487b3f012_b.jpg' />
         )
     }
 }
