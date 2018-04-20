5c4d497094fb117dfb38242e31a1d9d44726e66c
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-19 21:40:23 -0500

    ran yarn build

--- client/src/components/global/nav/index.js
+++ client/src/components/global/nav/index.js
@@ -10,7 +10,7 @@ class Nav extends Component {
                     <li><Link to='/login'>Login</Link></li>
                     <li><Link to='/userSearch'>User Search</Link></li>
                     <li><Link to='/create/destination'>Create Page</Link></li>
-                    <li><Link to='/destination/:id'>Destination Page</Link></li>
+                    {/* <li><Link to='/destination/:id'>Destination Page</Link></li> */}
                     <li><Link to='/profile'>Profile</Link></li>
 
                 </ul>
