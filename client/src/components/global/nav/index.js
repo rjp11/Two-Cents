64837665c6119cf06a3f79893613b5ad0e8bff9c
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-22 12:42:23 -0500

    user ID is passed to the Navbar on login

--- client/src/components/global/nav/index.js
+++ client/src/components/global/nav/index.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class Nav extends Component {
+    state = {
+        user_id: 1
+    }
+
+    componentDidMount = () => {
+        let userID = this.state.user_id;
+        axios.get('api/user_data').then((res) => {
+            console.log(res);
+            
+            this.setState({
+                user_id: res.data.id
+            })
+        })
+    }
+    
     render() {
         return(
             <div className='navbar navbar-inverse'>
@@ -12,6 +28,7 @@ class Nav extends Component {
                     <li><Link to='/create/destination'>Create Page</Link></li>
                     {/* <li><Link to='/destination/:id'>Destination Page</Link></li> */}
                     <li><Link to='/profile'>Profile</Link></li>
+                    <li> { this.state.user_id } </li>
 
                 </ul>
             </div>
