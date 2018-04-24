e6c079760fab9e8af96da75afff667ad106542f9
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-23 21:10:10 -0500

    adjusted model to match

--- models/poi.js
+++ models/poi.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING(2000),
             allowNull: false
         },
-        image_url: {
+        poi_image_url: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
